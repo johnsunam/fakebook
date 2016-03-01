18232a7cb32f60a947a75aa109d70474af3e9251
john <sunamjohn@gmail.com>
2016-03-01 17:40:47 +0545

    uploading image

--- client/components/main/statusform.jsx
+++ client/components/main/statusform.jsx
@@ -1,23 +1,42 @@
 Statusform=React.createClass({
+  getInitialState(){
+    return{
+      image:"",
+      filename:""
+    }
+  },
+  uploadFile(e){
+e.preventDefault();
+var that=this;
+FS.Utility.eachFile(e,function(file){
+  Images.insert(file,function(e,fileobj){
+    that.setState({image:fileobj.id,filename:fileobj.data.blob.name});
+  });
+});
+
+  },
   render(){
     return(
       <div className="panel panel-default">
       <div className="panel-heading">
       Update Status
       </div>
+      <form className="form center-block">
+      <input type="hidden" ref="imageid" className="form-control" value={this.state.image}/>
       <div className="panel-body">
-      <form>
       <div className="form-group">
       <textarea className="form-control" placeholder="what do u want to share?">
       </textarea>
+      <h3>{this.state.filename||''}</h3>
+      </div>
       </div>
       </form>
-      </div>
+
       <div className="panel-footer">
       <div>
       <ul className="pull-left list-inline">
       <li>
-      <input type="file" ref="file" className="filepicker" id="fie"/>
+      <input type="file" onChange={this.uploadFile} ref="file" className="filepicker" id="fie"/>
       </li>
       </ul>
       <button className="btn btn-primary">post
